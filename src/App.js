bec5d7ffba1689a918b895a5be991f31539a8028
Kim Young Min <kastori1990@gmail.com>
2023-03-08 13:11:49 +0900

    add Skills Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Skills from "./components/skills/Skills";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <main className="main">
         <Home />
         <About />
+        <Skills />
       </main>
     </>
   );
