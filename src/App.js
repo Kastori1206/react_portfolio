487d58edb6996550db5ca902d3474df17ee34e11
Kim Young Min <kastori1990@gmail.com>
2023-03-08 19:19:30 +0900

    add Services Component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
 
 export default function App() {
   return (
     <>
       <Header />
+
       <main className="main">
         <Home />
         <About />
         <Skills />
+        <Services />
       </main>
     </>
   );
