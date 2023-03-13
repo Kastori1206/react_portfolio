542d7b6b134aae30d1115d14bd141dd4ce6cdc78
Kim Young Min <kastori1990@gmail.com>
2023-03-13 13:38:35 +0900

    add config

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+export const DATABASE_ID = process.env.REACT_APP_NOTION_DATABASE_ID;
+export const TOKEN = process.env.REACT_APP_NOTION_TOKEN;
+export const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+export const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+export const PUBLICKEY = process.env.REACT_APP_PUBLICKEY;
