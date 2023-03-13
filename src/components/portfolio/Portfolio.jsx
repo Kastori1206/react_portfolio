46e963a8d6aed682dbaff8578391ffd7de42a488
Kim Young Min <kastori1990@gmail.com>
2023-03-13 14:37:22 +0900

    test

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -35,7 +35,10 @@ export default function Portfolio() {
   };
 
   const fetchData = async () => {
-    const res = await fetch(`v1/databases/${DATABASE_ID}/query`, options);
+    const res = await fetch(
+      `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+      options
+    );
 
     setProjects(await res.json());
   };
