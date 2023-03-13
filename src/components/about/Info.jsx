6d57194298b3dc6f723919d827b27dd8208147ff
Kim Young Min <kastori1990@gmail.com>
2023-03-13 13:35:33 +0900

    update about 페이지 수정

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -4,19 +4,19 @@ export default function info() {
   return (
     <div className="about__info grid">
       <div className="about__box">
-        <i class="bx bx-award about_icon"></i>
+        <i className="bx bx-award about_icon"></i>
         <h3 className="about__title">Experience</h3>
         <span className="about__subtitle">8 Years Working</span>
       </div>
 
       <div className="about__box">
-        <i class="bx bx-briefcase-alt about_icon"></i>
+        <i className="bx bx-briefcase-alt about_icon"></i>
         <h3 className="about__title">Completed</h3>
         <span className="about__subtitle">48 + Projects</span>
       </div>
 
       <div className="about__box">
-        <i class="bx bx-support about_icon"></i>
+        <i className="bx bx-support about_icon"></i>
         <h3 className="about__title">Support</h3>
         <span className="about__subtitle">Online 24/7</span>
       </div>
